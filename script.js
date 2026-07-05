4ccf235586157002a1ffb425b3fd7a439632d673
Linus Tucker <172833324+MrStarfleetCommand@users.noreply.github.com>
2026-07-05 00:23:46 -0400

    Strengthen color validation logic

--- script.js
+++ script.js
@@ -17,11 +17,11 @@
 		const fgColorG = parseInt(fgColor[3] + fgColor[4], 16);
 		const fgColorB = parseInt(fgColor[5] + fgColor[6], 16);
 
-		const finalR = Math.round((fgColorR - bgColorR * (1 - (opacity / 100))) / (opacity / 100))
-		const finalG = Math.round((fgColorG - bgColorG * (1 - (opacity / 100))) / (opacity / 100))
-		const finalB = Math.round((fgColorB - bgColorB * (1 - (opacity / 100))) / (opacity / 100))
+		const finalR = Math.round((fgColorR - bgColorR * (1 - (opacity / 100))) / (opacity / 100));
+		const finalG = Math.round((fgColorG - bgColorG * (1 - (opacity / 100))) / (opacity / 100));
+		const finalB = Math.round((fgColorB - bgColorB * (1 - (opacity / 100))) / (opacity / 100));
 
-		if (finalR > 255 || finalG > 255 || finalB > 255){
+		if (finalR > 255 || finalG > 255 || finalB > 255 || finalR < 0 || finalG < 0 || finalB < 0){
 			alert('No such color at this opacity setting');
 		} else {
 			alert(`rgb(${finalR} ${finalG} ${finalB} / ${opacity}%)`);
